60efc4f54ca1be9e1fe3fbcc9949f7d32239b6d7
Michael Gonzalez <michaelgonzalez0723@gmail.com>
2023-12-15 15:03:26 -0500

    css

--- src/component/About.js
+++ src/component/About.js
@@ -1,7 +1,5 @@
+import React from "react";
 function About(){
-
-
-
     return(
        <div 
        name="about"
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -95,6 +95,13 @@ function Navbar(){
                     >
                     Gituhub<FaGithub size={30}/>
                     </a>
+                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
+                        <a className='flex justify-between items-center w-full text-gray-300'
+                        href='/'
+                        >
+                            Resume<BsFillPersonLinesFill size={30}/>
+                        </a>
+                    </li>
                    </li>
                 </ul>
             </div>
